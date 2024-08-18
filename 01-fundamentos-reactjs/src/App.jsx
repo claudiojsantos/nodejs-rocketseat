a3e13d0762760a25734d0cc9def03140feb5a240
Cláudio Santos <claudio@sistnet.com.br>
2024-08-18 16:31:18 -0300

    react started

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,18 @@
+import Post from './Post'
+
+function App() {
+  return (
+    <div>
+      <Post 
+        author="Claudio Santos" 
+        description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur." 
+      />
+      <Post 
+        author="Aldo Campos" 
+        description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur."
+      />
+    </div>
+  )
+}
+
+export default App
