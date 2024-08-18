5122bcb9859f1fedb5a725cab79ae8b3da9648ca
Cláudio Santos <claudio@sistnet.com.br>
2024-08-18 18:23:22 -0300

    css global

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,13 @@
 import { Header } from './components/Header'
 import Post from './Post'
 
-import './styles.css'
+import './global.css'
 
 function App() {
   return (
     <div>
       <Header />
-      
+
       <Post 
         author="Claudio Santos" 
         description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur." 
