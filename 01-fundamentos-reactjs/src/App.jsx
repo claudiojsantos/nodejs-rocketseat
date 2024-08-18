5dd74065f9e65819d1405e075538e93d25258960
Cláudio Santos <claudio@sistnet.com.br>
2024-08-18 19:36:26 -0300

    Sidebar component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import Post from './Post'
 
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
@@ -8,14 +10,20 @@ function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Claudio Santos" 
-        description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur." 
-      />
-      <Post 
-        author="Aldo Campos" 
-        description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Claudio Santos" 
+            description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur." 
+          />
+          <Post 
+            author="Aldo Campos" 
+            description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur."
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1542831371-d531d36971e6?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src='https://github.com/claudiojsantos.png'></img>
+        <strong>Cláudio Santos</strong>
+        <spane>CEO</spane>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
