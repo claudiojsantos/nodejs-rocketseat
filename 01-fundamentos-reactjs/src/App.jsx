ed8527ac3cb1a572f4409954e13b0863aa0afe5c
Cláudio Santos <claudio@sistnet.com.br>
2024-09-18 01:11:33 -0300

    fix: import path

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import Post from './Post'
+import { Post } from './components/Post'
 
 import styles from './App.module.css'
 import './global.css'
@@ -13,12 +13,12 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Claudio Santos" 
-            description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur." 
+          <Post
+            author="Claudio Santos"
+            description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur."
           />
-          <Post 
-            author="Aldo Campos" 
+          <Post
+            author="Aldo Campos"
             description="Aliqua ipsum elit aute voluptate labore cupidatat officia nulla sunt amet pariatur."
           />
         </main>
